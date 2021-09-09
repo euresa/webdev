be41f9b1841c5bf13795f83dd69b04814aed53ae
Sam Eure <eure.samuel@gmail.com>
2021-09-09 12:26:31 -0400

    Finished first draft of the application

--- Applications/django-react-todoApp/backend/todo/models.py
+++ Applications/django-react-todoApp/backend/todo/models.py
@@ -4,8 +4,9 @@ from django.db import models
 
 class Todo(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    author = models.TextField()
     completed = models.BooleanField(default=False)
+    serialNumber = models.IntegerField()
 
     def __str__(self):
         return self.title
