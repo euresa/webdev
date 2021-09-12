b70fe57d631094596001f16eb75ff264ad725b45
Sam Eure <eure.samuel@gmail.com>
2021-09-12 12:20:46 -0400

    updates to hacker-stories

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,61 +1,75 @@
 import React from 'react';
 
 
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+  const handleSearch = (event) =>{
+    console.log(event.target.value);
+  }
 
-function App(){
   return(
     <div>
       <h1>My Hacker Stories</h1>
-      <Search/>
+      <Search onSearch={handleSearch}/>
       <hr/>
-      <List/>
+      <List stories={stories}/>
     </div>
   );
 }
 
-function List(){
-  return (
+const List = (props) => (
     <ul>
-      {list.map((item)=>{
-        return(
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        );
-      })}
+      {props.stories.map((story)=>(
+          <Item key={story.objectID} story={story}/>
+      ))}
     </ul>
-  );
-}
+);
+
+const Item = (props) => (
+  <li key={props.story.objectID}>
+    <span>
+      <a href={props.story.url}>{props.story.title}</a>
+    </span>
+    <span>{props.story.author}</span>
+    <span>{props.story.num_comments}</span>
+    <span>{props.story.points}</span>
+  </li>
+);
+
 /*New component here*/
-function Search(){
+const Search = (props) => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    props.onSearch(event);
+  };
+
   return(
     <div>
       <label htmlFor="search">Search:</label>
-      <input id='search' type="text"></input>
+      <input id='search' type="text" onChange={handleChange}></input>
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
     </div>
   );
 }
