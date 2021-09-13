249ef281f29947e2ee52dab772829de8525b66d1
Sam Eure <eure.samuel@gmail.com>
2021-09-13 18:57:39 -0400

    updates to search component

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(localStorage.getItem(key) ?? initialState);
@@ -11,32 +29,22 @@ const useSemiPersistentState = (key, initialState) => {
   return [value, setValue];
 };
 
-
 const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org',
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org',
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+
+  const [stories, setStories] = React.useState(initialStories);
 
   const handleSearch = (event) =>{
     setSearchTerm(event.target.value);
   };
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+  }
 
   const searchedStories = stories.filter((story) => 
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
@@ -45,42 +53,48 @@ const App = () => {
   return(
     <div>
       <h1>My Hacker Stories</h1>
-      <InputWithLabel
-        id='search'
-        label='Search'
-        type='text'
-        value={searchTerm}
-        onInputChange={handleSearch}
-      />
+      <InputWithLabel id='search' type='text' value={searchTerm} onInputChange={handleSearch}>
+        <strong>Search:</strong>
+      </InputWithLabel>
       <hr/>
-      <List stories={searchedStories}/>
+      <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
     </div>
   );
 }
 
-const List = ({stories}) => (
+const List = ({list, onRemoveItem}) => (
     <ul>
-      {stories.map((item)=>(
-          <Item key={item.objectID} item={item}/>
+      {list.map((item)=>(
+          <Item 
+            key={item.objectID}
+            item={item}
+            onRemoveItem={onRemoveItem}
+          />
       ))}
     </ul>
 );
 
-const Item = ({item}) => (
-  <li>
-    <>
-      <a href={item.url}>{item.title}</a>
-    </>
-    <>{item.author}</>
-    <>{item.num_comments}</>
-    <>{item.points}</>
-  </li>
-);
+const Item = ({item, onRemoveItem}) => (
+    <li>
+      <>
+        <a href={item.url}>{item.title}</a>
+      </>
+      <>{item.author}</>
+      <>{item.num_comments}</>
+      <>{item.points}</>
+      <>
+      <button type='button' onClick={() => onRemoveItem(item)}>
+          Dismiss
+        </button>
+      </>
+    </li>
+  );
+
 
 /*New component here*/
-const InputWithLabel = ({id, label, type, value, onInputChange}) => (
+const InputWithLabel = ({id, type='text', value, onInputChange, children}) => (
   <>
-    <label htmlFor={id}>{label}</label>
+    <label htmlFor={id}>{children}</label>
     &nbsp;
     <input id={id} type={type} value={value} onChange={onInputChange}/>
   </>
